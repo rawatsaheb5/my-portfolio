d1fc2a4e6651f5535ba2dec7d716be266ec38a41
rawatsaheb5 <diamondcut2004@gmail.com>
2023-08-18 10:42:24 +0530

    path update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,95 +1,95 @@
-import React from 'react'
-import './portfolio.css'
-import IMG1 from '../assets/portfolio1.jpg'
-import IMG2 from '../assets/portfolio2.jpg'
-import IMG3 from '../assets/portfolio3.jpg'
-import IMG4 from '../assets/portfolio4.jpg'
-import IMG5 from '../assets/portfolio5.png'
-import IMG6 from '../assets/portfolio6.jpg'
+// import React from 'react'
+// import './portfolio.css'
+// import IMG1 from '../assets/portfolio1.jpg'
+// import IMG2 from '../assets/portfolio2.jpg'
+// import IMG3 from '../assets/portfolio3.jpg'
+// import IMG4 from '../assets/portfolio4.jpg'
+// import IMG5 from '../assets/portfolio5.png'
+// import IMG6 from '../assets/portfolio6.jpg'
 
-const Portfolio = () => {
-  return (
-    <section id='portfolio'>
-      <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
+// const Portfolio = () => {
+//   return (
+//     <section id='portfolio'>
+//       <h5>My Recent Work</h5>
+//       <h2>Portfolio</h2>
 
-      <div className='container portfolio_container'>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG1}  alt=''/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio_item-cta'>
-          <a href='https://github.com' className='btn' target='_blank'>Github</a>
-          <a href='https://' className='btn btn-primary' target='_blank'>Live Demo</a>
+//       <div className='container portfolio_container'>
+//         <article className='portfolio_item'>
+//           <div className='portfolio_item-image'>
+//             <img src={IMG1}  alt=''/>
+//           </div>
+//           <h3>This is a portfolio item title</h3>
+//           <div className='portfolio_item-cta'>
+//           <a href='https://github.com' className='btn' target='_blank'>Github</a>
+//           <a href='https://' className='btn btn-primary' target='_blank'>Live Demo</a>
 
-          </div>
+//           </div>
           
-        </article>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG2}  alt=''/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio_item-cta'>
-          <a href='https://github.com' className='btn' target='_blank'>Github</a>
-          <a href='https://' className='btn btn-primary' target='_blank'>Live Demo</a>
+//         </article>
+//         <article className='portfolio_item'>
+//           <div className='portfolio_item-image'>
+//             <img src={IMG2}  alt=''/>
+//           </div>
+//           <h3>This is a portfolio item title</h3>
+//           <div className='portfolio_item-cta'>
+//           <a href='https://github.com' className='btn' target='_blank'>Github</a>
+//           <a href='https://' className='btn btn-primary' target='_blank'>Live Demo</a>
 
-          </div>
+//           </div>
           
-        </article>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG3}  alt=''/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio_item-cta'>
-          <a href='https://github.com' className='btn' target='_blank'>Github</a>
-          <a href='https://' className='btn btn-primary' target='_blank'>Live Demo</a>
+//         </article>
+//         <article className='portfolio_item'>
+//           <div className='portfolio_item-image'>
+//             <img src={IMG3}  alt=''/>
+//           </div>
+//           <h3>This is a portfolio item title</h3>
+//           <div className='portfolio_item-cta'>
+//           <a href='https://github.com' className='btn' target='_blank'>Github</a>
+//           <a href='https://' className='btn btn-primary' target='_blank'>Live Demo</a>
 
-          </div>
+//           </div>
           
-        </article>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG4}  alt=''/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio_item-cta'>
-          <a href='https://github.com' className='btn' target='_blank'>Github</a>
-          <a href='https://' className='btn btn-primary' target='_blank'>Live Demo</a>
+//         </article>
+//         <article className='portfolio_item'>
+//           <div className='portfolio_item-image'>
+//             <img src={IMG4}  alt=''/>
+//           </div>
+//           <h3>This is a portfolio item title</h3>
+//           <div className='portfolio_item-cta'>
+//           <a href='https://github.com' className='btn' target='_blank'>Github</a>
+//           <a href='https://' className='btn btn-primary' target='_blank'>Live Demo</a>
 
-          </div>
+//           </div>
           
-        </article>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG5}  alt=''/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio_item-cta'>
-          <a href='https://github.com' className='btn' target='_blank'>Github</a>
-          <a href='https://' className='btn btn-primary' target='_blank'>Live Demo</a>
+//         </article>
+//         <article className='portfolio_item'>
+//           <div className='portfolio_item-image'>
+//             <img src={IMG5}  alt=''/>
+//           </div>
+//           <h3>This is a portfolio item title</h3>
+//           <div className='portfolio_item-cta'>
+//           <a href='https://github.com' className='btn' target='_blank'>Github</a>
+//           <a href='https://' className='btn btn-primary' target='_blank'>Live Demo</a>
 
-          </div>
+//           </div>
           
-        </article>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG6}  alt=''/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio_item-cta'>
-          <a href='https://github.com' className='btn' target='_blank'>Github</a>
-          <a href='https://' className='btn btn-primary' target='_blank'>Live Demo</a>
+//         </article>
+//         <article className='portfolio_item'>
+//           <div className='portfolio_item-image'>
+//             <img src={IMG6}  alt=''/>
+//           </div>
+//           <h3>This is a portfolio item title</h3>
+//           <div className='portfolio_item-cta'>
+//           <a href='https://github.com' className='btn' target='_blank'>Github</a>
+//           <a href='https://' className='btn btn-primary' target='_blank'>Live Demo</a>
 
-          </div>
+//           </div>
           
-        </article>
-      </div>
+//         </article>
+//       </div>
 
-    </section>
-  )
-}
+//     </section>
+//   )
+// }
 
-export default Portfolio
+// export default Portfolio
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,44 +1,44 @@
-import React from 'react'
-import './testimonials.css';
-import data from './reviews'
+// import React from 'react'
+// import './testimonials.css';
+// import data from './reviews'
 
-import { Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Pagination } from 'swiper';
+// import { Swiper, SwiperSlide } from 'swiper/react';
 
-import 'swiper/css';
+// import 'swiper/css';
 
-import 'swiper/css/pagination';
+// import 'swiper/css/pagination';
 
 
-const Testimonials = () => {
-  return (
-    <section id='testimonials'>
-      <h5>Review from Clients</h5>
-      <h2>Testimonials</h2>
-      <Swiper modules={[ Pagination]}
-      spaceBetween={40}
-      slidesPerView={1} 
-      pagination={{ clickable: true }}
-      className='container testimonials_container'>
-        {
-          data.map(({ avatar, name, review }, index) => {
+// const Testimonials = () => {
+//   return (
+//     <section id='testimonials'>
+//       <h5>Review from Clients</h5>
+//       <h2>Testimonials</h2>
+//       <Swiper modules={[ Pagination]}
+//       spaceBetween={40}
+//       slidesPerView={1} 
+//       pagination={{ clickable: true }}
+//       className='container testimonials_container'>
+//         {
+//           data.map(({ avatar, name, review }, index) => {
             
-            return (
-              <SwiperSlide key={index} className='testimonials'>
-          <div className='client_avatar'>
-            <img src={avatar} alt='AVATAR ONE' />
+//             return (
+//               <SwiperSlide key={index} className='testimonials'>
+//           <div className='client_avatar'>
+//             <img src={avatar} alt='AVATAR ONE' />
             
-          </div>
-          <h5 className='client_name'>{name}</h5>
-              <small className='client_review'>{ review}</small>
+//           </div>
+//           <h5 className='client_name'>{name}</h5>
+//               <small className='client_review'>{ review}</small>
 
-        </SwiperSlide>)
-          })
-        }
+//         </SwiperSlide>)
+//           })
+//         }
         
-      </Swiper>
-    </section>
-  )
-}
+//       </Swiper>
+//     </section>
+//   )
+// }
 
-export default Testimonials
+// export default Testimonials
